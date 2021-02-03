7761e8b34337508dc2bc9b3ffc93bdec6c425608
Артем <cooler292@mail.ru>
2021-02-03 20:16:29 +0300

    Тест соединения

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from authapp.models import User
 
 class UserLoginForm(AuthenticationForm):
@@ -29,3 +29,16 @@ class UserRegisterForm(UserCreationForm):
             self.fields['password2'].widget.attrs['placeholder'] = 'Подтвердите пароль'
             for field_name, field in self.fields.items():
                 field.widget.attrs['class'] = 'form-control py-4'
+
+class UserProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'avatar')
+
+        def __init__(self, *args, **kwargs):
+            super(UserProfileForm, self).__init__(*args, **kwargs)
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'form-control py-4'
+                self.fields['username'].widget.attrs['readonly'] = True
+                self.fields['email'].widget.attrs['readonly'] = True
+                self.fields['avatar'].widget.attrs['class'] = 'custom-file-input'
